5a3720f80666aa5caaed12aa5ccb18e31e8130be
Thiago <thiagooffice2@gmail.com>
2021-12-16 16:01:00 -0300

    Criando lista de tarefas

--- src/pages/board/index.tsx
+++ src/pages/board/index.tsx
@@ -1,13 +1,63 @@
 import styles from './styles.module.scss'
 import { GetServerSideProps } from 'next'
 import { getSession } from 'next-auth/client'
-
+import {useState , FormEvent} from 'react'
+import firebase from '../../services/firebaseConnection'
+import { format} from 'date-fns'
+import Link from 'next/link'
+ 
 
 import Head from 'next/head'
 import {FiPlus, FiCalendar, FiEdit2, FiTrash, FiClock} from 'react-icons/fi'
 import { SupportButton } from '../../components/SuporteButton'
 
-export default function Board(){
+interface BoardProps{
+  user:{
+    id: string,
+    nome: string
+  }
+}
+
+export default function Board({ user }: BoardProps){
+
+  const [input, setInput] = useState("")
+  const [taskList, setTaskList] = useState([])
+
+  async function handleAddTask(e: FormEvent){
+    e.preventDefault()
+
+    if(input === ""){
+      alert("Preencha alguma tarefa!")
+      return
+    }
+
+    await firebase.firestore().collection("tarefas")
+    .add({
+      created: new Date(),
+      tarefa: input,
+      userId: user.id,
+      nome: user.nome
+    }
+    )
+    .then((doc)=>{
+      let data ={
+        id: doc.id,
+        created: new Date(),
+        createdFormated: format(new Date(), "dd MMMM yyy"),
+        tarefa: input,
+        userId: user.id,
+        nome: user.nome
+      }
+      setTaskList([...taskList, data])
+      setInput("")
+    })
+    .catch((err)=>{
+      console.log("Erro ao cadastrar", err)
+    })
+      
+
+  }
+
     return(
         <>
         <Head>
@@ -16,10 +66,12 @@ export default function Board(){
         
         <main className={styles.container}>
           
-          <form>
+          <form onSubmit={handleAddTask}>
             <input 
             type="text"
             placeholder='Digite a sua tarefa...'
+            value={input}
+            onChange={(e)=> setInput(e.target.value)}
             />
             <button type='submit'>
             <FiPlus size={25} color='#17181f' /> 
@@ -30,28 +82,34 @@ export default function Board(){
           <h1>Você tem 2 tarefas</h1>
 
           <section>
-            <article className={styles.taskList}>
 
-          <p>Aprender criar projetos usando Next JS e aplicando firebase como back.</p>
+            {taskList.map(task => (
+              <article className={styles.taskList}>
 
-          <div className={styles.actions}>
-            <div>
-              <div>
-          <FiCalendar size={20} color='#ffb800'/>
-          <time>17 Julho 2021</time>
+              <Link href={`/board/${task.id}`}>
+              <p>{task.tarefa}</p>
+              </Link>
+    
+              <div className={styles.actions}>
+                <div>
+                  <div>
+              <FiCalendar size={20} color='#ffb800'/>
+              <time>{task.createdFormated}</time>
+                  </div>
+                <button>
+                  <FiEdit2 size={20} color='#fff'/>
+                  <span>Editar</span>
+                </button>
+                </div>
+    
+                <button>
+                  <FiTrash size={20} color='#ff3636'/>
+                  <span>Excluir</span>
+                </button>
               </div>
-            <button>
-              <FiEdit2 size={20} color='#fff'/>
-              <span>Editar</span>
-            </button>
-            </div>
-
-            <button>
-              <FiTrash size={20} color='#ff3636'/>
-              <span>Excluir</span>
-            </button>
-          </div>
-            </article>
+                </article>
+            ))}
+            
           </section>
 
         </main>
@@ -87,11 +145,14 @@ export const getServerSideProps: GetServerSideProps = async ({req })=>{
     }
   }
 
-  console.log(session.user)
+  const user={
+    nome: session?.user.name,
+    id:session?.id
+  }
 
   return{
     props:{
-
+      user
     }
   }
 }
