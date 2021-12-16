1ee94fa612756dbaf82d2af6b3d4a11fb3b32cec
Thiago <thiagooffice2@gmail.com>
2021-12-16 16:52:08 -0300

    Buscando tarefas

--- src/pages/board/index.tsx
+++ src/pages/board/index.tsx
@@ -11,17 +11,26 @@ import Head from 'next/head'
 import {FiPlus, FiCalendar, FiEdit2, FiTrash, FiClock} from 'react-icons/fi'
 import { SupportButton } from '../../components/SuporteButton'
 
+type TaskList={
+  id: string,
+  created: string | Date,
+  createdFormated?: string,
+  tarefa: string,
+  userId: string,
+  nome: string
+}
 interface BoardProps{
   user:{
     id: string,
     nome: string
   }
+  data: string
 }
 
-export default function Board({ user }: BoardProps){
+export default function Board({ user, data }: BoardProps){
 
   const [input, setInput] = useState("")
-  const [taskList, setTaskList] = useState([])
+  const [taskList, setTaskList] = useState<TaskList[]>(JSON.parse(data))
 
   async function handleAddTask(e: FormEvent){
     e.preventDefault()
@@ -79,7 +88,7 @@ export default function Board({ user }: BoardProps){
             </button>
           </form>
 
-          <h1>Você tem 2 tarefas</h1>
+          <h1>Você tem {taskList.length} {taskList.length === 1 ? "Tarefa!" : "Tarefas!"}</h1>
 
           <section>
 
@@ -145,6 +154,18 @@ export const getServerSideProps: GetServerSideProps = async ({req })=>{
     }
   }
 
+  const tasks = await firebase.firestore().collection("tarefas")
+  .where("userId", "==", session?.id)
+  .orderBy("created", "asc").get()
+
+  const data = JSON.stringify(tasks.docs.map(u =>{
+    return{
+      id: u.id,
+      createdFormated: format(u.data().created.toDate(), "dd MMMM yyy"),
+      ...u.data(),
+    }
+  }))
+
   const user={
     nome: session?.user.name,
     id:session?.id
@@ -152,7 +173,8 @@ export const getServerSideProps: GetServerSideProps = async ({req })=>{
 
   return{
     props:{
-      user
+      user,
+      data
     }
   }
 }
